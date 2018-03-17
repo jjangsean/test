5aa682ba7c4d9e35ee93f3db7746106a9b1ea0d5
jjangsean <4066898@naver.com>
2018-03-18 00:12:59 +0900

    test

---
+++ Test/src/calculator/code/Calculator.java
@@ -0,0 +1,16 @@
+package calculator.code;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int add(int a, int b) {
+		int result;
+		result = a+b;
+		return result;
+	}
+
+}
---
+++ Test/src/calculator/test/Calulator_test.java
@@ -0,0 +1,17 @@
+package calculator.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import calculator.code.Calculator;
+
+class Calulator_test {
+
+	@Test
+	void testAdd() {
+		Calculator cal = new Calculator();
+		assertEquals(3,cal.add(1, 2));
+	}
+
+}
